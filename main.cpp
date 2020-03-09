e4cf06b0af616805c1513abd6eef1eef8519b578
iamkroot <kroot.patel@gmail.com>
2020-03-09 11:05:06 +0530

    Add proper measurement of execution time for the algorithms

--- main.cpp
+++ main.cpp
@@ -4,25 +4,47 @@
 #include "KosarajuFinder.tcc"
 #include "Utils.h"
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        std::cout << "Provide a file path to a csv containing list of edges." << std::endl;
-        return 0;
-    } else if (argc > 2) {
-        std::cout << "Too many arguments!" << std::endl;
-        return 1;
-    }
-
+Graph<int> readGraph(const std::string &path) {
     std::vector<std::pair<int, int>> edgeList;
-    edgeList = readEdgeList(argv[1]);
+    try {
+        edgeList = readEdgeList(path);
+    } catch (std::runtime_error &err) {
+        std::cerr << "Error parsing " << path << std::endl << err.what() << std::endl;
+        return {};
+    }
     Graph<int> g{};
     for (const auto &edge : edgeList) {
         g.addEdge(edge.first, edge.second);
     }
-    std::cout << "Loaded graph with " << g.getNumVertices() << " vertices and "
+    std::cout << "Loaded graph from " << path << " with " << g.getNumVertices() << " vertices and "
               << g.getNumEdges() << " edges." << std::endl;
+    return g;
+}
+
+void compareAlgos(const Graph<int>& g) {
+    Stopwatch<> stopwatch;
     auto KosarajuSCC = Kosaraju::getSCC(g);
-    std::cout << "Kosaraju: "<< KosarajuSCC.size() << " components." << std::endl;
+    auto KosarajuTime = stopwatch.stop();
+    std::cout << "Kosaraju: " << KosarajuSCC.size() << " components. Time: " << KosarajuTime << "μs." << std::endl;
+
+    stopwatch.start();
     auto DCSCC = DCSC::getSCC(g);
-    std::cout << "Divide and Conquer: "<< DCSCC.size() << " components." << std::endl;
+    auto DCSCTime = stopwatch.stop();
+    std::cout << "Divide and Conquer: " << DCSCC.size() << " components. Time: " << DCSCTime << "μs." << std::endl;
+
+    std::cout << "Runtime ratio: " << (double) DCSCTime / KosarajuTime << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Provide a file path to a csv containing list of edges." << std::endl;
+        return 0;
+    }
+    for (int i = 1; i < argc; ++i) {
+        auto g = readGraph(argv[i]);
+        if (g.getNumVertices() == 0) {
+            continue;
+        }
+        compareAlgos(g);
+    }
 }
