3cf2fa72da1e0561813cca3e4f75ab2e4677f7e0
iamkroot <kroot.patel@gmail.com>
2020-03-11 13:34:36 +0530

    Print components to stdout

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include "Graph.h"
 #include "DCSCFinder.tcc"
 #include "KosarajuFinder.tcc"
@@ -21,7 +22,7 @@ Graph<int> readGraph(const std::string &path) {
     return g;
 }
 
-void compareAlgos(const Graph<int>& g) {
+void compareAlgos(const Graph<int> &g) {
     Stopwatch<> stopwatch;
     auto KosarajuSCC = Kosaraju::getSCC(g);
     auto KosarajuTime = stopwatch.stop();
@@ -35,16 +36,49 @@ void compareAlgos(const Graph<int>& g) {
     std::cout << "Runtime ratio: " << (double) DCSCTime / KosarajuTime << std::endl;
 }
 
+void writeSCC(const std::vector<Graph<int>> &scc) {
+    std::cout << "Number of components: " << scc.size() << std::endl;
+    int i = 1;
+    for (const auto &comp : scc) {
+        std::cout << "Component " << i << ":";
+        for (const auto &vert: comp.getVertices()) {
+            std::cout << " " << vert;
+        }
+        std::cout << std::endl;
+        i++;
+    }
+}
+
 int main(int argc, char *argv[]) {
+    std::stringstream help;
+    help << "Usage:" << std::endl
+         << "scc_algos [compare|getSCC] DATASET_PATH [DATASET_PATH...]" << std::endl
+         << std::endl
+         << "compare\t For each graph, measure the runtimes of Kosaraju and Divide and Conquer algorithms"
+         << std::endl
+         << "getSCC\t For each graph, print the vertices Strongly Connected Components to stdout" << std::endl
+         << std::endl
+         << "The DATASET_PATH should be a path (no spaces) to a CSV file containing edges of the graph in the form: src,dest"
+         << std::endl;
+
     if (argc < 2) {
-        std::cout << "Provide a file path to a csv containing list of edges." << std::endl;
+        std::cout << help.str();
         return 0;
     }
-    for (int i = 1; i < argc; ++i) {
+    std::string op = argv[1];
+    if (op != "compare" && op != "getSCC") {
+        std::cerr << help.str();
+        return -1;
+    }
+    for (int i = 2; i < argc; ++i) {
         auto g = readGraph(argv[i]);
         if (g.getNumVertices() == 0) {
             continue;
         }
-        compareAlgos(g);
+        if (op == "compare") {
+            compareAlgos(g);
+        } else if (op == "getSCC") {
+            writeSCC(Kosaraju::getSCC(g));
+        }
     }
 }
